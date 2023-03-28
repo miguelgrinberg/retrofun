750879ea9f6ee3f6d0ba39dc1d67e78a03814679
Miguel Grinberg <miguel.grinberg@gmail.com>
2023-03-29 00:06:00 +0100

    Use the newer DeclarativeBase parent class

--- chapter3/db.py
+++ chapter3/db.py
@@ -1,16 +1,20 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+
+class Model(DeclarativeBase):
+    metadata = MetaData(naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    })
+
 
 load_dotenv()
 
 engine = create_engine(os.environ['DATABASE_URL'])
-Model = declarative_base(metadata=MetaData(naming_convention={
-  "ix": "ix_%(column_0_label)s",
-  "uq": "uq_%(table_name)s_%(column_0_name)s",
-  "ck": "ck_%(table_name)s_%(constraint_name)s",
-  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-  "pk": "pk_%(table_name)s",
-}))
 Session = sessionmaker(engine)
--- chapter7/db.py
+++ chapter7/db.py
@@ -2,18 +2,22 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import MetaData, event, inspect
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Model(DeclarativeBase):
+    metadata = MetaData(naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    })
+
 
 load_dotenv()
 
 engine = create_async_engine(os.environ['DATABASE_URL'])
-Model = declarative_base(metadata=MetaData(naming_convention={
-  "ix": "ix_%(column_0_label)s",
-  "uq": "uq_%(table_name)s_%(column_0_name)s",
-  "ck": "ck_%(table_name)s_%(constraint_name)s",
-  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-  "pk": "pk_%(table_name)s",
-}))
 Session = async_sessionmaker(engine, expire_on_commit=False)
 
 
